234c5decfa3c792ab1fc62b63e9a0f568027ddfb
abdul17097 <abdul17097@gmail.com>
2024-03-14 02:37:09 -0700

    make project setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
